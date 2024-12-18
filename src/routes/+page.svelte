e68ff54da5a169d0e7075732ea906c52ef410272
Jiho Lee <54468974+2jiho@users.noreply.github.com>
2024-12-18 15:47:35 +0900

    개발중인 페이지에서 prettier 설정을 추가하고, 코드를 작성중입니다.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  let message = "환영합니다!";
+
+  function handleClick() {
+    alert(message);
+  }
+</script>
+
+<main>
+  <img src="https://cryptologos.cc/logos/bitcoin-btc-logo.svg" id="icon" alt="ICON" />
+  <p id="price">TESTPRICE</p>
+  <div id="change">
+    <p id="change-value">TESTPERCENT</p>
+    <p id="change-label">24h</p>
+  </div>
+</main>
+
+<style>
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 100vh;
+    width: 100vw;
+    background-color: #000;
+    color: #ffffff;
+    font-family: Arial, sans-serif;
+    margin: 0;
+    overflow: hidden;
+    user-select: none; /* 텍스트 선택 방지 */
+  }
+  #icon {
+    height: 15vh;
+  }
+</style>
