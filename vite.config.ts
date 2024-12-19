24eeb479ebb1520a16ba7c76aeb91c12d7df92f8
Jiho Lee <54468974+2jiho@users.noreply.github.com>
2024-12-20 01:47:00 +0900

    feat: PWA 지원 추가
    - SvelteKitPWA 플러그인을 사용하여 PWA 지원 추가
    - name, short_name, start_url, display, background_color, theme_color, description, orientation 등의 매니페스트 설정 구성
    - 다양한 기기와 해상도를 위한 여러 아이콘 추가
    - 캐싱 전략을 위한 workbox 및 injectManifest 구성

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,61 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import { SvelteKitPWA } from "@vite-pwa/sveltekit";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [
+    sveltekit(),
+    SvelteKitPWA({
+      manifest: {
+        name: "TickerDisplay",
+        short_name: "TickerDisplay",
+        start_url: "/",
+        display: "fullscreen",
+        background_color: "#000000",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+          },
+          {
+            src: "/favicon-16x16.png",
+            sizes: "16x16",
+            type: "image/png",
+          },
+          {
+            src: "/favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
+          },
+          {
+            src: "/favicon.ico",
+            sizes: "48x48",
+            type: "image/x-icon",
+          },
+          {
+            src: "/favicon.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+          },
+        ],
+      },
+      injectManifest: {
+        globPatterns: ["client/**/*.{js,css,ico,png,svg,webp,woff,woff2}"],
+      },
+      workbox: {
+        globPatterns: ["client/**/*.{js,css,ico,png,svg,webp,woff,woff2}"],
+      },
+    }),
+  ],
 });
