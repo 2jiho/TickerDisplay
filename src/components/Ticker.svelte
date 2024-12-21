eac841d40a7e87dda3b0dbd14414c82d4b2d4c98
Jiho Lee <54468974+2jiho@users.noreply.github.com>
2024-12-22 03:31:35 +0900

    fix: 로딩 오류 시 기본 아이콘 사용하도록 수정
    - 컴포넌트에서 텍스트 크기 조절 제거

--- src/components/Ticker.svelte
+++ src/components/Ticker.svelte
@@ -1,51 +1,31 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-
+  // svg icon (https://www.svgrepo.com/collection/essential-collection/)
   let { tickerData } = $props();
 
   let iconElement: HTMLImageElement;
   let priceElement: HTMLParagraphElement;
   let changeElement: HTMLParagraphElement;
 
+  let useDefaultIcon = false;
+  const defaultIconUrl = "./icons/error.svg";
+
   async function fetchIcon() {
     // 아이콘 로드
     const iconTicker = tickerData.market.split("-")[1].toLowerCase();
     const iconUrl = `https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons/svg/color/${iconTicker}.svg`;
-    if (iconUrl === iconElement.src) {
+    if (iconUrl === iconElement.src || useDefaultIcon) {
       return;
     }
     try {
       const response = await fetch(iconUrl);
-      if (response.ok) {
-        iconElement.setAttribute("src", iconUrl);
-      } else {
+      if (!response.ok) {
         console.log("Icon not found:", iconUrl, "using default icon.");
-        iconElement.setAttribute(
-          "src",
-          "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons/svg/color/generic.svg",
-        );
+        useDefaultIcon = true;
       }
+      iconElement.setAttribute("src", useDefaultIcon ? defaultIconUrl : iconUrl);
     } catch (error) {
       console.error("Error fetching icon:", error);
     }
-  }
-
-  function autoScaleFontSize(element: HTMLParagraphElement) {
-    /**
-     * 요소의 폰트 크기 자동 조절
-     * - 부모 요소에 맞게 요소의 크기를 조절
-     */
-    const parent = element.parentElement;
-    if (parent) {
-      const scaleWidth = parent.clientWidth / element.clientWidth;
-      const scaleHeight = parent.clientHeight / element.clientHeight;
-      const scale = Math.min(scaleWidth, scaleHeight);
-      (element as HTMLElement).style.transform = `scale(${scale})`;
-    }
-  }
-
-  function handleAutoScaleFontSize() {
-    ([priceElement, changeElement] as HTMLParagraphElement[]).forEach(autoScaleFontSize);
   }
 
   function updatePrice() {
@@ -58,69 +38,43 @@
     changeElement.style.color = change > 0 ? "lightgreen" : change < 0 ? "lightcoral" : "white";
   }
 
-  onMount(() => {
-    window.addEventListener("resize", handleAutoScaleFontSize);
-    return () => {
-      window.removeEventListener("resize", handleAutoScaleFontSize);
-    };
-  });
-
   $effect(() => {
     updatePrice();
     fetchIcon();
-    handleAutoScaleFontSize();
   });
 </script>
 
 <div class="container">
-  <div class="icon">
-    <img bind:this={iconElement} class="icon-image" src="" alt="ICON" />
-  </div>
-  <div class="price">
-    <p bind:this={priceElement} class="price-text">LOADING...</p>
-  </div>
-  <div class="change">
-    <p bind:this={changeElement} class="change-text">0.00%</p>
-  </div>
+  <img bind:this={iconElement} class="icon-image" src="" alt="ICON" />
+  <p bind:this={priceElement} class="price-text">LOADING...</p>
+  <p bind:this={changeElement} class="change-text">0.00%</p>
 </div>
 
 <style>
   .container {
-    display: flex;
+    display: grid;
+    grid-template-columns: 100px 300px 100px;
     justify-content: center;
     align-items: center;
-    width: 100%;
-    height: 100%;
-    margin: 10px;
-  }
-  .icon {
-    flex: 2;
-  }
-  .price {
-    flex: 6;
-  }
-  .change {
-    flex: 2;
-  }
-  .icon,
-  .price,
-  .change {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
+    width: 500px;
+    height: 100px;
   }
   .icon-image {
     width: 100%;
     height: 100%;
     object-fit: contain;
   }
+  .price-text {
+    font-size: 30px;
+  }
+  .change-text {
+    font-size: 20px;
+  }
   .price-text,
   .change-text {
     margin: 0;
-    display: inline-block; /* 요소를 inline-block으로 설정하여 부모 요소에 맞게 크기 조절 */
     font-family: Arial, sans-serif;
     color: #ffffff;
-    padding: 16px;
+    text-align: center;
   }
 </style>
